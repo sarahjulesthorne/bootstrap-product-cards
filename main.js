052d4ee9dd46a6eff09f538a3ee1bc9ca8157a08
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-03-19 19:45:38 -0500

    pushing functions

--- main.js
+++ main.js
@@ -1,1 +1,75 @@
-console.log('Hey!');
+const products = [
+    {
+        name: 'product 1',
+        description: 'A very awesome product',
+        imageURL: '',
+        size: '4 in. by 4 in.',
+        weight: '5 lb.',
+        price1: '1.99',
+        price2: '1.49',
+        price3: '.99',
+        validUntil: '04/01/2019',
+    },
+    {
+        name: 'product 2',
+        description: 'A very awesome product',
+        imageURL: '',
+        size: '4 in. by 4 in.',
+        weight: '5 lb.',
+        price1: '1.99',
+        price2: '1.49',
+        price3: '.99',
+        validUntil: '04/01/2019',
+    },
+    {
+        name: 'product 3',
+        description: 'A very awesome product',
+        imageURL: '',
+        size: '4 in. by 4 in.',
+        weight: '5 lb.',
+        price1: '1.99',
+        price2: '1.49',
+        price3: '.99',
+        validUntil: '04/01/2019',
+    },
+    {
+        name: 'product 4',
+        description: 'A very awesome product',
+        imageURL: '',
+        size: '4 in. by 4 in.',
+        weight: '5 lb.',
+        price1: '1.99',
+        price2: '1.49',
+        price3: '.99',
+        validUntil: '04/01/2019',
+    },
+    {
+        name: 'product 5',
+        description: 'A very awesome product',
+        imageURL: '',
+        size: '4 in. by 4 in.',
+        weight: '5 lb.',
+        price1: '1.99',
+        price2: '1.49',
+        price3: '.99',
+        validUntil: '04/01/2019',
+    },
+];
+
+const printToDom = (divId, textToPrint) => {
+const selectedDiv = document.getElementById(divId);
+selectedDiv.innerHTML = textToPrint;
+};
+
+const domStringBuilder = () => {
+let domString = '';
+products.forEach((product) => {
+domString += `<h2>${product.name}</h2>`;
+})
+printToDom('cardContainer', domString);
+};
+
+const init = () => {
+domStringBuilder();
+};
+init();
